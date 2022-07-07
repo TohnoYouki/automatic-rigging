cf02461af2b486843cc5ca26150e908baca34558
Youki Tohno <33323998+TohnoYouki@users.noreply.github.com>
2022-07-07 14:55:15 +0800

    Update dae.py

--- datasets/parse_tool/dae/dae.py
+++ datasets/parse_tool/dae/dae.py
@@ -6,12 +6,6 @@ from .reader import Reader
 class DAE:
     @staticmethod
     def transform(scene, upaxis):
-        min_v = np.min(scene.vertices, 0)
-        max_v = np.max(scene.vertices, 0)
-        middle = (min_v + max_v) / 2
-        scale = np.max(max_v - min_v)
-        scene.vertices = (scene.vertices - middle) / scale
-        scene.joint_position = (scene.joint_position - middle) / scale
         if upaxis == 'Y_UP':
             matrix = np.array([[1, 0, 0], [0, 0, 1], [0, -1, 0]])
         elif upaxis == 'Z_UP':
