0b3cc309471b3b8bbb30013b7e0fbc67556dc82a
Youki Tohno <33323998+TohnoYouki@users.noreply.github.com>
2022-08-31 22:41:04 +0800

    Add files via upload

--- datasets/input_output/parse/__init__.py
+++ datasets/input_output/parse/__init__.py
@@ -3,3 +3,5 @@ from .gltf.gltf import GLTF
 from .smd.smd import SMDReader
 from .gltf.scene import Scene as GLTFScene
 from .smd.rigging_mesh import RiggingMesh as SMDScene
+from .smd.static_mesh import StaticMesh as SMDStaticMesh
+from .smd.vertex_animation import VertexAnimation as SMDVertexAnimation
