d4a0c6fc6e5d07db65773fce7bbe17f47187b0e9
Youki Tohno <33323998+TohnoYouki@users.noreply.github.com>
2022-08-31 22:24:20 +0800

    Add files via upload

--- utils/multiprocess.py
+++ utils/multiprocess.py
@@ -4,7 +4,7 @@ from multiprocessing import Pool
 from inputimeout import inputimeout, TimeoutOccurred
 
 def wrap_fun(params):
-    number, fun, lock, input_queue, output_queue = params
+    number, fun, lock, input_queue, output_queue, error_queue = params
     while True:
         try:
             index, data = input_queue.get(False)
@@ -14,6 +14,9 @@ def wrap_fun(params):
             if isinstance(e, Empty): break
             else:
                 print('process', number, 'has error:', e) 
+                lock.acquire()
+                error_queue.put([index, data], False)
+                lock.release()
                 continue
         else:
             lock.acquire()
@@ -25,17 +28,19 @@ def prepare(fun, data, number):
     lock = manager.Lock()
     input_queue = manager.Queue(len(data))
     output_queue = manager.Queue(len(data))
+    error_queue = manager.Queue(len(data))
     for i in range(len(data)):
         input_queue.put([i, data[i]], False)
-    params = [[i, fun, lock, input_queue, output_queue] 
+    params = [[i, fun, lock, input_queue, output_queue, error_queue] 
                for i in range(number)]
     pool = Pool(processes = number)
     result = pool.map_async(wrap_fun, params)
-    return manager, lock, input_queue, output_queue, result, pool
+    return manager, lock, input_queue, output_queue, error_queue, result, pool
 
 def multi_process(fun, data, number):
     prepare_result = prepare(fun, data, number)
-    manager, lock, input_queue, output_queue, result, pool = prepare_result
+    manager, lock, input_queue, output_queue, \
+        error_queue, result, pool = prepare_result
     while not result.ready():
         try: c = inputimeout(timeout = 10)
         except TimeoutOccurred: c = None
@@ -44,7 +49,9 @@ def multi_process(fun, data, number):
             pool.terminate()
             lock.release()
             break
-    result = []
+    result, error = [], []
     while not output_queue.empty():
         result.append(output_queue.get(False))
-    return result
+    while not error_queue.empty():
+        error.append(error_queue.get(False))
+    return result, error
